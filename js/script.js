15a410771dc2321b2bef12e3a837bcfbd588dcab
Omar Abdelkream <omarrabdoo00@gmail.com>
2024-05-08 17:22:40 +0300

    have added local storage to the project so i can keep my data

--- js/script.js
+++ js/script.js
@@ -7,18 +7,30 @@ addButton.onclick = function () {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
     listsContainer.appendChild(li);
+    window.localStorage.setItem("task", inputBox.value);
     let span = document.createElement("span");
     span.innerHTML = "\u00d7";
     li.appendChild(span);
   } else {
     alert(`You Need to Write Your Tasks!`);
   }
+  saveData();
 };
 
 listsContainer.addEventListener("click", function (el) {
   if (el.target.tagName === "LI") {
     el.target.classList.toggle("checked");
+    saveData();
   } else if (el.target.tagName === "SPAN") {
     el.target.parentElement.remove();
+    saveData();
   }
 });
+
+function saveData() {
+  window.localStorage.setItem("data", listsContainer.innerHTML);
+}
+function showData() {
+  listsContainer.innerHTML = window.localStorage.data;
+}
+showData();
